e03dc33938fbde961a378910c955ffe878c46ad1
bhavyasharma <bhavya5d.sharma@gmail.com>
2025-10-06 16:03:51 +0530

    feat: connect frontend to Render backend

--- client/src/utils/customFetch.js
+++ client/src/utils/customFetch.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const customFetch = axios.create({
-  baseURL: '/api/v1',
+  baseURL: 'https://jobtrail-backend.onrender.com/api/v1',
 });
 
 export default customFetch;
